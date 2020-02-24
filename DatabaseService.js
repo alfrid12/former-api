bf7ba514be2b8bd9f6bdfb70d6757df70ddcd946
Alex Fridman <alexander.fridman@libertymutual.com>
2020-02-24 08:51:28 -0500

    Added a couple basic routes and a database service class

---
+++ DatabaseService.js
@@ -0,0 +1,53 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const databaseName = 'test';
+
+
+const insertObjectIntoCollection = (object, collectionName, callback) => {
+    MongoClient.connect(url, (error, client) => {
+        if (error) console.log(error);
+        else {
+            const db = client.db(databaseName);
+            const collection = db.collection(collectionName);
+            collection.insertOne(object, (error, result) => {
+                callback(error, result);
+                client.close();
+            });
+        }
+    });
+}
+
+const getFormByFormId = (formId, callback) => {
+    MongoClient.connect(url, (error, client) => {
+        if (error) console.log(error);
+        else {
+            const db = client.db(databaseName);
+            const collection = db.collection('forms');
+            collection.findOne({ formId }, (error, result) => {
+                callback(error, result);
+                client.close();
+            });
+        }
+    });
+}
+
+const getEntireCollection = (collectionName, callback) => {
+    MongoClient.connect(url, (error, client) => {
+        if (error) console.log(error);
+        else {
+            const db = client.db(databaseName);
+            const collection = db.collection(collectionName);
+            collection.find({}).toArray((error, result) => {
+                callback(error, result);
+                client.close();
+            });
+        }
+    });
+}
+
+module.exports = {
+    insertObjectIntoCollection,
+    getEntireCollection,
+    getFormByFormId
+};
